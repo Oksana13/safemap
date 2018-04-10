82a9f092da0496769becdb95d119d3e6fa530c14
Oksana_Klimova <oksana_klimova@epam.com>
2018-04-10 15:44:27 +0300

    Update chart data

---
+++ src/assets/data2.js
@@ -0,0 +1,10 @@
+export const DrilldownData = {
+  series: [{
+    name: 'Corrected',
+    data: [5, 7]
+  }, {
+    name: 'Non corrected',
+    data: [10, 5]
+  }]
+
+};
--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,12 +1,16 @@
 <template>
   <section class="charts">
     <vue-highcharts :Highcharts="Highcharts" :options="drilldownOptions" ref="drilldownChart"></vue-highcharts>
+    <p>{{drilldownOptions}}</p>
+    <button @click="load">load</button>
+    <button @click="reload">reload</button>
   </section>
 </template>
 <script>
   import VueHighcharts from "vue2-highcharts";
   import Drilldown from "highcharts/modules/drilldown.js";
   import * as data from "../assets/data.js";
+  import * as data2 from "../assets/data2.js";
   import Highcharts from "highcharts";
 
   Drilldown(Highcharts);
@@ -24,6 +28,16 @@
         Highcharts: Highcharts,
         drilldownOptions: data.DrilldownData
       };
+    },
+    methods: {
+      load: function () {
+          this.drilldownOptions = {};
+          this.drilldownOptions = data2.DrilldownData;
+      },
+      reload: function () {
+        this.drilldownOptions = {};
+        this.drilldownOptions = data.DrilldownData;
+      }
     }
   };
 </script>
